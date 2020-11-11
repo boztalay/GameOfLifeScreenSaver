3f56337df04a2b46d6b46c11a4d205471e9faa1c
Ben Oztalay <boztalay@gmail.com>
2020-11-10 19:35:58 -0800

    Easing curve for the wave

--- GameOfLifeScreenSaver/GameOfLifeScreenSaver/CellView.swift
+++ GameOfLifeScreenSaver/GameOfLifeScreenSaver/CellView.swift
@@ -9,8 +9,8 @@ import Cocoa
 
 class CellView: NSView {
     
-    private static let deadColor = NSColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0).cgColor
-    private static let aliveColor = NSColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0).cgColor
+    private static let deadColor  = NSColor(red: 0.05, green: 0.05, blue: 0.05, alpha: 1.0).cgColor
+    private static let aliveColor = NSColor(red: 0.90, green: 0.90, blue: 0.95, alpha: 1.0).cgColor
 
     private let stepPeriod: CGFloat
     
@@ -21,17 +21,30 @@ class CellView: NSView {
     }
     
     func update(cell: GameOfLifeCellState) {
+        // (0, 0)
+//        let waveOrigin = CGPoint.zero
+//        let maxDistanceFromWaveOrigin = waveOrigin.distance(to: self.superview!.frame.size.point)
+        
+        // Middle of left edge
         let waveOrigin = CGPoint(
-            x: self.superview!.frame.width / 2.0,
+            x: 0.0,
             y: self.superview!.frame.height / 2.0
         )
+        let maxDistanceFromWaveOrigin = waveOrigin.distance(to: self.superview!.frame.size.point)
+        
+        // Center
+//        let waveOrigin = CGPoint(
+//            x: self.superview!.frame.width / 2.0,
+//            y: self.superview!.frame.height / 2.0
+//        )
+//        let maxDistanceFromWaveOrigin = waveOrigin.distance(to: self.superview!.frame.size.point)
 
         let distanceFromWaveOrigin = waveOrigin.distance(to: self.frame.origin)
-        let maxDistanceFromWaveOrigin = waveOrigin.distance(to: self.superview!.frame.size.point)
         let waveDistanceDelayProportion = distanceFromWaveOrigin / maxDistanceFromWaveOrigin
+        let easedDistanceDelayProportion = self.eaze(waveDistanceDelayProportion)
         
         let cellStateDelay = cell.isAlive ? 0.0 : (self.stepPeriod * 0.15)
-        let delay = cellStateDelay + (self.stepPeriod * 0.55 * waveDistanceDelayProportion)
+        let delay = cellStateDelay + (self.stepPeriod * 0.55 * easedDistanceDelayProportion)
         let animationStartTime = DispatchTime.now() + Double(delay)
         
         DispatchQueue.main.asyncAfter(deadline: animationStartTime) {
@@ -50,6 +63,11 @@ class CellView: NSView {
             layer.backgroundColor = newColor
             CATransaction.commit()
         }
+    }
+    
+    private func eaze(_ x: CGFloat) -> CGFloat {
+        // www.easings.net
+        return sin((x * CGFloat.pi) / 2.0)
     }
     
     required init?(coder: NSCoder) {
