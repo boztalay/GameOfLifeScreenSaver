a720cef9c93e19805abf6ea85b18fb3a979d627c
Ben Oztalay <boztalay@gmail.com>
2023-01-05 18:37:10 -0800

    Fix how the cell's corner radius is set

--- GameOfLifeScreenSaver/GameOfLifeScreenSaver/CellContentView.swift
+++ GameOfLifeScreenSaver/GameOfLifeScreenSaver/CellContentView.swift
@@ -1,5 +1,5 @@
 //
-//  StupidView.swift
+//  CellContentView.swift
 //  GameOfLifeScreenSaver
 //
 //  Created by Ben Oztalay on 11/17/20.
@@ -8,24 +8,16 @@
 import Cocoa
 
 class CellContentView: NSView {
-    
-    private let backgroundColor: CGColor
-    private let cornerRadius: CGFloat
 
     init(frame frameRect: NSRect, backgroundColor: CGColor, cornerRadius: CGFloat) {
-        self.backgroundColor = backgroundColor
-        self.cornerRadius = cornerRadius
         super.init(frame: frameRect)
+        
+        self.wantsLayer = true
+        self.layer?.backgroundColor = backgroundColor
+        self.layer?.cornerRadius = cornerRadius
+        self.layer?.cornerCurve = .continuous
     }
 
-    override func makeBackingLayer() -> CALayer {
-        let layer = super.makeBackingLayer()
-        layer.backgroundColor = self.backgroundColor
-        layer.cornerRadius = self.cornerRadius
-        layer.cornerCurve = .continuous
-        return layer
-    }
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
